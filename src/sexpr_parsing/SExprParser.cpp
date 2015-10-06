902d1a560947fa314faf69a65b13f38fca908eb5
Raphael Isemann <teemperor@gmail.com>
2015-10-06 13:20:39 +0200

    Small refactoring to SExprParser. We also throw the a UnknownDataAtEndOfStream exception now in the parse method

--- src/sexpr_parsing/SExprParser.cpp
+++ src/sexpr_parsing/SExprParser.cpp
@@ -2,4 +2,58 @@
 
 namespace wasm_module {
 
+    void SExprParser::parseValues(SExpr &parent, bool allowsEndOfStream) {
+
+        bool exit = false;
+
+        while (!exit) {
+            if (allowsEndOfStream) {
+                if (stream_.reachedEnd()) {
+                    break;
+                }
+            }
+
+            stream_.trimWhitespace();
+
+            if (stream_.peekChar() == '(') {
+                stream_.popChar();
+                parseValues(parent.addChild(), false);
+            } else if (stream_.peekChar() == ')') {
+                stream_.popChar();
+                exit = true;
+            } else {
+                std::string word;
+
+                while (true) {
+                    char c = stream_.popChar();
+                    if (!stream_.isWhitespace(c)) {
+                        if (c == ')') {
+                            exit = true;
+                            parent.addChild(word);
+                            break;
+                        } else {
+                            word.push_back(c);
+                        }
+                    } else {
+                        parent.addChild(word);
+                        break;
+                    }
+                }
+
+            }
+        }
+
+    }
+
+    SExpr SExprParser::parse(bool allowExitBeforeEOF = false) {
+        SExpr root;
+        parseValues(root, true);
+        if (!allowExitBeforeEOF && !stream_.reachedEnd()) {
+            throw UnknownDataAtEndOfStream();
+        }
+        return root;
+    }
+
+    SExprParser::SExprParser(CharacterStream &stream) : stream_(stream) {
+    }
 }
--- src/sexpr_parsing/SExprParser.h
+++ src/sexpr_parsing/SExprParser.h
@@ -14,61 +14,12 @@ namespace wasm_module {
 
         CharacterStream &stream_;
 
-        void parseValues(SExpr &parent, bool allowsEndOfStream) {
-
-            bool exit = false;
-
-            while (!exit) {
-                if (allowsEndOfStream) {
-                    if (stream_.reachedEnd()) {
-                        break;
-                    }
-                }
-
-                stream_.trimWhitespace();
-
-                if (stream_.peekChar() == '(') {
-                    stream_.popChar();
-                    parseValues(parent.addChild(), false);
-                } else if (stream_.peekChar() == ')') {
-                    stream_.popChar();
-                    exit = true;
-                } else {
-                    std::string word;
-
-                    while (true) {
-                        char c = stream_.popChar();
-                        if (!stream_.isWhitespace(c)) {
-                            if (c == ')') {
-                                exit = true;
-                                parent.addChild(word);
-                                break;
-                            } else {
-                                word.push_back(c);
-                            }
-                        } else {
-                            parent.addChild(word);
-                            break;
-                        }
-                    }
-
-                }
-            }
-
-        }
+        void parseValues(SExpr &parent, bool allowsEndOfStream);
 
     public:
-        SExprParser(CharacterStream &stream) : stream_(stream) {
-        }
+        SExprParser(CharacterStream &stream);
 
-        SExpr parse(bool allowExitBeforeEOF = false) {
-            SExpr root;
-            parseValues(root, true);
-            if (!allowExitBeforeEOF && !stream_.reachedEnd()) {
-                throw UnexpectedEndOfCharacterStream();
-            }
-            return root;
-        }
+        SExpr parse(bool allowExitBeforeEOF = false);
 
     };
 
